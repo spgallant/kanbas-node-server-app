9163f0d49a79e5570caaf0aff9c4096f10d17201
spgallant <sarahpgallant@gmail.com>
2024-07-10 12:27:59 -0400

    thru a5 2.7

---
+++ App.js
@@ -0,0 +1,11 @@
+//const express = require('express')
+import express from 'express';
+const app = express()
+
+app.get('/hello', (req, res) => {res.send('Life is Good!')})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Full Stack Development!')})
+
+    
+app.listen(4000)
