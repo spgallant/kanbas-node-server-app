b28164a5ebfad83b6d10e442fb4f09b08e8499c8
spgallant <sarahpgallant@gmail.com>
2024-07-11 10:10:54 -0400

    a5 lab thru 3.4

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 //const express = require('express')
 
-import express from 'express';
+import express from 'express'; // Import the Express module
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 
-const app = express();
+const app = express(); // Create an Express application
 app.use(express.json());
 
 Lab5(app);
 Hello(app);
 
-    
+// Start the server and listen on the defined port w/ given port number
 app.listen(process.env.PORT || 4000)
--- Lab5/index.js
+++ Lab5/index.js
@@ -2,6 +2,7 @@
 import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
 
 
 export default function Lab5(app) {
@@ -18,5 +19,6 @@ export default function Lab5(app) {
     PathParameters(app);
     QueryParameters(app);
     WorkingWithObjects(app);
+    WorkingWithArrays(app);
   };
   
