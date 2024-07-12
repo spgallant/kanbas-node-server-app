0666df7a54db6092186e900266fe9a566b0c1e8f
spgallant <sarahpgallant@gmail.com>
2024-07-12 10:21:53 -0400

    a5 kanbas thru 4.3.2 w/ Assignments C

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -2,6 +2,26 @@ import db from "../Database/index.js";
 
 export default function AssignmentRoutes(app) {
 
+    // Create operation for assignments with POST
+    // url may need ~/assignments/:aid
+    app.post("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params; //parse cid required parameter
+
+        // new assignment info is in request body, cid from req. param
+        const newAssignment = {
+          ...req.body,
+          course: cid,
+          _id: new Date().getTime().toString(),
+        };
+
+        db.assignments.push(newAssignment); //push new module to db.modules array
+
+        res.send(newAssignment); //reply to client with new module
+    });
+
+
+
+
     // Read operation for assignments with GET
     app.get("/api/courses/:cid/assignments", (req, res) => {
         const { cid } = req.params;
--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -2,6 +2,25 @@ import db from "../Database/index.js";
 
 export default function ModuleRoutes(app) {
 
+
+    // Create operation for modules with POST
+    app.post("/api/courses/:cid/modules", (req, res) => {
+        const { cid } = req.params; //parse cid required parameter
+
+        // new module info is in request body, cid from req. param
+        const newModule = {
+          ...req.body,
+          course: cid,
+          _id: new Date().getTime().toString(),
+        };
+
+        db.modules.push(newModule); //push new module to db.modules array
+
+        res.send(newModule); //reply to client with new module
+    }); 
+
+
+    
     // Read operation for modules with GET
     app.get("/api/courses/:cid/modules", (req, res) => {
         const { cid } = req.params;
