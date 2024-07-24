cc6a296c94f196c639182a5f430c21031b198be1
spgallant <sarahpgallant@gmail.com>
2024-07-24 15:42:10 -0400

    a6 working thru 3.6.2

--- Users/routes.js
+++ Users/routes.js
@@ -4,19 +4,44 @@ let currentUser = null;
 
 export default function UserRoutes(app) {
   const createUser = async (req, res) => { };
-  const deleteUser = async (req, res) => { };
+
+  const deleteUser = async (req, res) => { 
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
 
 
 
   const findAllUsers = async (req, res) => {  
-    const users = await dao.findAllUsers();
+
+    const { role, name } = req.query; // parse role from query string, name from "
+    if (role) { // if role matches 
+      const users = await dao.findUsersByRole(role); // use dao to retrieve users with that role
+      res.json(users); // respond with json of filtered user list
+      return; //exit the fn
+    }
+
+    if (name) { //if name matches
+      const users = await dao.findUsersByPartialName(name);  // use dao to retrieve users with that name
+      res.json(users);
+      return;
+    }
+
+
+    const users = await dao.findAllUsers(); // else find all users regardless of role?
     res.json(users);
+    
+  };
+
+  
+  const findUserById = async (req, res) => { 
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
   };
 
 
 
 
-  const findUserById = async (req, res) => { };
   const updateUser = async (req, res) => { };
   const signup = async (req, res) => { };
   const signin = async (req, res) => { };
